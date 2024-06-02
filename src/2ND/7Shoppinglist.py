b0a36dd857909f0c6c84b5e118024ff4a3523074
DDRAV <dariusz.dravnel@gmail.com>
2024-06-02 14:03:35 +0300

    create Shoppinglist

--- src/2ND/7Shoppinglist.py
+++ src/2ND/7Shoppinglist.py
@@ -4,3 +4,7 @@
 	#7.3 Pridėkite viešuosius metodus add_item, kad pridėtumėte prekę su nurodytu kiekiu, remove_item, kad pašalintumėte prekę, ir get_total_items, kad grąžintumėte bendrą krepšelio prekių skaičių.
 	#7.4 Užtikrinkite, kad metodai add_item ir remove_item atnaujintų krepšelį ir apskaičiuotų bendrą kiekį.
 	#7.5 Sukurkite ShoppingCart objektą, pridėkite ir pašalinkite elementus ir gaukite bendrą elementų skaičių.
+
+class ShoppingCart:
+    def __init__(self,_items: dict):
+        self._items = _items
